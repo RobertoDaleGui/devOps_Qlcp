2d4da2d34ec8ef4f6ccebbcca5c755cd347bc884
Roberto D'Alessandro Guimarães <robertodalegui@gmail.com>
2021-03-13 16:39:47 -0300

    FEAT: implementado AWS Secrets

--- src/apis/aws-api.js
+++ src/apis/aws-api.js
@@ -1,9 +1,12 @@
 const AWS = require('aws-sdk');
+const region = process.env.AWS_DEPLOY_REGION
 
 const dynamoDb = new AWS.DynamoDB.DocumentClient({
   api_version: process.env.AWS_DYNAMODB_VERSION,
-  region: process.env.AWS_DEPLOY_REGION
+  region: region
 })
+
+const secretMng = new AWS.SecretsManager({ region: region })
 
 if (process.env.IS_LOCAL || process.env.IS_OFFLINE || process.env.SERVERLESS_TEST_ROOT || process.env.NODE_ENV === 'test') {
   const credentials = new AWS.SharedIniFileCredentials({ profile: 'Roberlove' });
@@ -26,9 +29,14 @@ async function dynamoUpdate (data) {
   return dynamoDb.update(data).promise()
 }
 
+function getSecretMng (data) {
+  return secretMng.getSecretValue({ SecretId: data }).promise()
+}
+
 module.exports = {
   dynamoGet,
   dynamoPut,
   dynamoScan,
-  dynamoUpdate
+  dynamoUpdate,
+  getSecretMng
 }
